463be686b10b7e01105ab53079075af6e3aea5fb
Arthur Neto <arthursan.neto@hotmail.com>
2020-08-03 23:18:22 -0300

    Initial commit

---
+++ src/server/Template.Application/AppServiceBase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace Template.Application
+{
+    public abstract class AppServiceBase<TRepository> : IDisposable where TRepository : IDisposable
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        protected readonly TRepository _repository;
+        protected readonly IMapper _mapper;
+
+        protected AppServiceBase(TRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        protected AppServiceBase(TRepository repository, IMapper mapper, IUnitOfWork unitOfWork)
+            : this(repository, mapper)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<int> CommitAsync()
+        {
+            return _unitOfWork.CommitAsync();
+        }
+
+        public void Dispose()
+        {
+            if (_unitOfWork != null) { _unitOfWork.Dispose(); }
+            _repository.Dispose();
+        }
+    }
+}
